a6e4be0073853c709cfd317aad21820d14a4fb9e
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2020-08-03 17:17:07 -0700

    fixed linting errors

--- cvapipe/steps/pca_path_cells/pca_path_cells.py
+++ cvapipe/steps/pca_path_cells/pca_path_cells.py
@@ -5,6 +5,9 @@ import logging
 from pathlib import Path
 from typing import Dict, List, Optional, Union
 
+import numpy as np
+import pandas as pd
+
 from datastep import Step, log_run_params
 from .utils import scan_pc_for_cells
 
@@ -20,30 +23,32 @@ class PcaPathCells(Step):
         self,
         direct_upstream_tasks: List["Step"] = [],
         config: Optional[Union[str, Path, Dict[str, str]]] = None,
-        filepath_columns=["dataframe_loc"]
+        filepath_columns=["dataframe_loc"],
     ):
         super().__init__(direct_upstream_tasks=direct_upstream_tasks, config=config)
 
     @log_run_params
     def run(
         self,
-        pca_csv_loc=Path("/allen/aics/assay-dev/MicroscopyOtherData/Viana/forCaleb/variance/05202020_Align-IND_Chirality-OFF/manifest.csv"),
-        pcs=[1,2,3,4,5,6,7,8],
-        path=np.array([-2.0, -1.5, -1.0, -0.5,  0.0,  0.5,  1.0,  1.5,  2.0]),
+        pca_csv_loc=Path(
+            "/allen/aics/assay-dev/MicroscopyOtherData/Viana/forCaleb/variance/05202020_Align-IND_Chirality-OFF/manifest.csv"
+        ),
+        pcs=[1, 2, 3, 4, 5, 6, 7, 8],
+        path=np.array([-2.0, -1.5, -1.0, -0.5, 0.0, 0.5, 1.0, 1.5, 2.0]),
         dist_cols=[
-            'DNA_MEM_PC1',
-            'DNA_MEM_PC2',
-            'DNA_MEM_PC3',
-            'DNA_MEM_PC4',
-            'DNA_MEM_PC5',
-            'DNA_MEM_PC6',
-            'DNA_MEM_PC7',
-            'DNA_MEM_PC8'
+            "DNA_MEM_PC1",
+            "DNA_MEM_PC2",
+            "DNA_MEM_PC3",
+            "DNA_MEM_PC4",
+            "DNA_MEM_PC5",
+            "DNA_MEM_PC6",
+            "DNA_MEM_PC7",
+            "DNA_MEM_PC8",
         ],
         metric="euclidean",
         id_col="CellId",
         N_cells=3,
-        **kwargs
+        **kwargs,
     ):
         """
         Look through PCA embeddings of cells and find groups of cells closest to PC axes.
@@ -97,15 +102,17 @@ class PcaPathCells(Step):
         -------
         result: self.manifest
         """
-        
+
         self.manifest = pd.DataFrame(columns=["PC", "dataframe_path"])
-        pc_path_dir = (self.step_local_staging_dir / "pc_paths").mkdir(parents=True, exist_ok=True)
-        
+        pc_path_dir = (self.step_local_staging_dir / "pc_paths").mkdir(
+            parents=True, exist_ok=True
+        )
+
         # TODO change this filepath location to an upstream step
         df_pca = pd.read_csv(pca_csv_loc)
-        
+
         for pc in pcs:
-            df_cells = scan_pc_for_cells( 
+            df_cells = scan_pc_for_cells(
                 df_pca,
                 pc=pc,
                 path=path,
@@ -114,10 +121,10 @@ class PcaPathCells(Step):
                 id_col=id_col,
                 N_cells=N_cells,
             )
-        
+
             fpath = pc_path_dir / f"pca_{pc}_path_cells.csv"
             df_cells.to_csv(fpath, index=False)
-            
-            self.manifest = self.manifest.append({"PC":pc, "dataframe_path":df_loc})
-        
+
+            self.manifest = self.manifest.append({"PC": pc, "dataframe_path": fpath})
+
         return self.manifest
