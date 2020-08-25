59b3a14f3345089454877535059bb9c7658b9071
Rory Donovan-Maiye <rorydm@alleninstitute.org>
2020-08-24 19:16:35 -0700

    mean raw comparison refactor passes linting

--- cvapipe/steps/multi_res_struct_compare/multi_res_struct_compare.py
+++ cvapipe/steps/multi_res_struct_compare/multi_res_struct_compare.py
@@ -7,16 +7,16 @@ from typing import Dict, List, Optional, Union
 
 import pandas as pd
 
-import matplotlib.pyplot as plt
-import seaborn as sns
 from aics_dask_utils import DistributedHandler
-from dask.distributed import performance_report
-
-from aicsimageio import AICSImage
 
 from datastep import Step, log_run_params
 
-from .utils import pyramid_correlation, draw_pairs
+from .utils import (
+    compute_distance_metric,
+    make_pairs_df,
+    clean_up_results,
+    make_plot,
+)
 
 ###############################################################################
 
@@ -32,89 +32,6 @@ class MultiResStructCompare(Step):
         config: Optional[Union[str, Path, Dict[str, str]]] = None,
     ):
         super().__init__(direct_upstream_tasks=direct_upstream_tasks, config=config)
-
-    def compute_distance_metric(
-        self,
-        row: pd.Series,
-        mdata_cols: List[str],
-    ) -> Union[pd.DataFrame, None]:
-
-        log.info(
-            "Beginning pairwise metric computation"
-            f" between cells {row.CellId_i}"
-            f" and {row.CellId_j}"
-        )
-
-        # i and j are abused as variables and str names
-        inds = {
-            "i": int(row.CellId_i),
-            "j": int(row.CellId_j)
-        }
-
-        # get data for cells i and j
-        gfp_i = masked_gfp_store[row.CellId_i]
-        gfp_j = masked_gfp_store[row.CellId_j]
-        
-        # multi-res comparison
-        pyr_corrs = pyramid_correlation(gfp_i, gfp_j, func=np.mean)
-    
-        # comparison when one input is permuted (as baseline correlation)
-        gfp_i_shuf = gfp_i.copy().flatten()
-        np.random.shuffle(gfp_i_shuf)
-        gfp_i_shuf = gfp_i_shuf.reshape(gfp_i.shape)
-        pyr_corrs_permuted = pyramid_correlation(gfp_i_shuf, gfp_j, func=np.mean)
-    
-        # grab correlations at each res in a df
-        df_tmp_corrs = pd.DataFrame()
-        for k,v in sorted(pyr_corrs.items()):
-            tmp_stat_dict = {
-                "Resolution (micrometers)":px_size*k,
-                "Pearson Correlation":v,
-                "Pearson Correlation permuted":pyr_corrs_permuted[k]
-            }
-            df_tmp_corrs = df_tmp_corrs.append(tmp_stat_dict, ignore_index=True)
-        
-        # label stats with cell ids
-        df_tmp_corrs["CellId_i"] = row.CellId_i
-        df_tmp_corrs["CellId_j"] = row.CellId_j
-
-        # and append row metadata
-        df_row_tmp = row.to_frame().T
-        df_row_tmp = df_row_tmp.merge(df_tmp_corrs)
-
-        log.info(
-            f"Completed pairwise metric between cells {row_i.CellId}"
-            f" and {row_j.CellId}"
-        )
-
-        return df_row_tmp
-
-    def loop_distance_metric(
-        self,
-        row_index_i: int,
-        row_i: pd.Series,
-        row_index_j: int,
-        row_j: pd.Series,
-        mdata_cols: List,
-    ) -> Union[pd.DataFrame, None]:
-
-        if row_i["StructureShortName"] == row_j["StructureShortName"]:
-            if row_index_j > row_index_i:
-                assert row_i["CellId"] != row_j["CellId"]
-                # Save Dask report every time row index i is 1
-                # This is arbitrary, just want to see the performance
-                # once per loop
-                if row_index_i == 1:
-                    with performance_report(filename="dask-report.html"):
-                        df_row_tmp = self.compute_distance_metric(
-                            row_index_i, row_i, row_index_j, row_j, mdata_cols,
-                        )
-                else:
-                    df_row_tmp = self.compute_distance_metric(
-                        row_index_i, row_i, row_index_j, row_j, mdata_cols,
-                    )
-
-                return df_row_tmp
 
     @log_run_params
     def run(
@@ -139,7 +56,7 @@ class MultiResStructCompare(Step):
             "CellImage3DPath",
         ],
         px_size=0.29,
-        image_dims_crop_size = (64, 160, 96),
+        image_dims_crop_size=(64, 160, 96),
         par_dir=Path("/allen/aics/modeling/ritvik/projects/actk/"),
         input_csv_loc=Path("local_staging/singlecellimages/manifest.csv"),
         distributed_executor_address: Optional[str] = None,
@@ -162,7 +79,8 @@ class MultiResStructCompare(Step):
                 "Nucleolus (Granular Component)",
             ]
         N_pairs_per_struct: int
-            How many pairs of GFP instances per tagged structure to sample from the bigger input dataset.
+            How many pairs of GFP instances per tagged structure to sample
+            from the bigger input dataset.
             Default: 100
         mdata_cols: List[str]
             Which columns from the input dataset to include n the output as metadata
@@ -197,38 +115,16 @@ class MultiResStructCompare(Step):
         self._input_csv_loc = input_csv_loc
         self._px_size = px_size
 
-        # Handle dataset provided as string or path
-        if isinstance(par_dir / input_csv_loc, (str, Path)):
-            df = Path(par_dir / input_csv_loc).expanduser().resolve(strict=True)
+        # Read dataset
+        df = pd.read_csv(par_dir / input_csv_loc)
 
-            # Read dataset
-            df = pd.read_csv(par_dir / input_csv_loc)
-
-            
         # subset down to only N_pairs_per_struct
-        dataset = pd.DataFrame()
-
-        for struct in structs:
-            try:
-                df_struct = df[df.StructureShortName == struct]
-                pair_inds = draw_pairs(df_struct.index, n_pairs=N_pairs_per_struct)
-                inds_i = np.array(list(pair_inds))[:,0]
-                inds_j = np.array(list(pair_inds))[:,1]
-                df_struct_i = df_struct.loc[inds_i]
-                df_struct_j = df_struct.loc[inds_j]
-                for (i,j) in pair_inds:
-                    row_i = df_struct.loc[i]
-                    row_j = df_struct.loc[j]
-                    row = row_i[mdata_cols].add_suffix("_i").append(row_j[mdata_cols].add_suffix("_j"))
-                    dataset = dataset.append(row, ignore_index=True)
-            except Exception as e:
-                log.info(
-                    f"Not enough {struct} rows to get {N_pairs_per_struct} samples"
-                    f" Error {e}."
-                )
-                break
-        assert np.all(dataset["CellId_i"] != dataset["CellId_j"])
-        dataset = dataset.reset_index(drop=True)
+        dataset = make_pairs_df(
+            df,
+            structs=structs,
+            N_pairs_per_struct=N_pairs_per_struct,
+            mdata_cols=mdata_cols,
+        )
 
         # Empty futures list
         distance_metric_futures = []
@@ -236,107 +132,51 @@ class MultiResStructCompare(Step):
 
         # Process each row
         with DistributedHandler(distributed_executor_address) as handler:
-            for this_row_index, this_row in dataset.iterrows():
-                # Start processing
-                distance_metric_future = handler.client.map(
-                    self.loop_distance_metric,
-                    # Convert dataframe iterrows into two lists of items to iterate over
-                    # One list will be row index
-                    # One list will be the pandas series of every row
-                    *zip(*list(dataset.iterrows())),
-                    # Keep the other row (row j) constant as we
-                    # loop through the dataset (row i)
-                    [this_row_index for i in range(len(dataset))],
-                    [this_row for i in range(len(dataset))],
-                    [mdata_cols for i in range(len(dataset))],
-                )
+            # Start processing
+            distance_metric_future = handler.client.map(
+                compute_distance_metric,
+                [row for i, row in dataset.iterrows()],
+                [mdata_cols for i in range(len(dataset))],
+                [px_size for i in range(len(dataset))],
+                [image_dims_crop_size for i in range(len(dataset))],
+                [par_dir for i in range(len(dataset))],
+            )
 
-                distance_metric_futures.append(distance_metric_future)
-
-                result = handler.gather(distance_metric_future)
-                distance_metric_results.append(result)
-
-            # This seems to be a lot slower and clogs a single core
-            #  Collect futures
-            # distance_metric_results = [
-            #     handler.gather(f) for f in distance_metric_futures
-            # ]
+            distance_metric_futures.append(distance_metric_future)
+            result = handler.gather(distance_metric_future)
+            distance_metric_results.append(result)
 
         # Assemble final dataframe
-        df_final = pd.DataFrame()
-        for dataframes in distance_metric_results:
-            for corr_dataframe in dataframes:
-                # corr_dataframe is None if
-                # row_i["StructureShortName"] != row_j["StructureShortName"]
-                # or row_index_j > row_index_i
-                if corr_dataframe is not None:
-                    df_final = df_final.append(corr_dataframe)
-
-        log.info(f"Assembled Parwise metrics dataframe with shape {df_final.shape}")
-        
-        # fix up final pairwise dataframe
-        df_final = df_final.reset_index(drop=True)
-        df_final = df_final.rename(
-            columns={"StructureShortName_i": "StructureShortName"}
-        ).drop(columns="StructureShortName_j")
-        df_final["Pearson Correlation gain over random"] = df_final["Pearson Correlation"] - df_final["Pearson Correlation permuted"]
+        df_final = clean_up_results(distance_metric_results)
 
         # make a manifest
         self.manifest = pd.DataFrame(columns=["Description", "path"])
 
         # where to save outputs
-        pairwise_dir = self.step_local_staging_dir / "pairwise_metrics"
-        pairwise_dir.mkdir(parents=True, exist_ok=True)
-        plots_dir = self.step_local_staging_dir / "pairwise_plots"
-        plots_dir.mkdir(parents=True, exist_ok=True)
+        pairwise_loc = (
+            self.step_local_staging_dir
+            / "pairwise_metrics"
+            / "multires_pairwise_similarity.csv"
+        )
+        pairwise_loc.mkdir(parents=True, exist_ok=True)
+        plot_loc = (
+            self.step_local_staging_dir
+            / "pairwise_plots"
+            / "multi_resolution_image_correlation.png"
+        )
+        plot_loc.mkdir(parents=True, exist_ok=True)
 
         # save pairwise dataframe to csv
-        df_final.to_csv(pairwise_dir / "multires_pairwise_similarity.csv", index=False)
+        df_final.to_csv(pairwise_loc, index=False)
         self.manifest = self.manifest.append(
-            {
-                "Description": "raw similarity scores",
-                "path": pairwise_dir / "multires_pairwise_similarity.csv",
-            },
+            {"Description": "raw similarity scores", "path": pairwise_loc},
             ignore_index=True,
         )
 
         # make a plot
-        sns.set(style="ticks", rc={"lines.linewidth": 1.0})
-        fig = plt.figure(figsize=(10,7))
-
-        ax = sns.pointplot(
-            x="Resolution (micrometers)",
-            y="Pearson Correlation gain over random",
-            hue="StructureShortName",
-            data=df_pairwise_corrs,
-            ci=95,
-            capsize=.2,
-            palette="Set2",
-        )
-        ax.legend(
-            loc='upper left',
-            bbox_to_anchor=(0.05, 0.95),
-            ncol=1,
-            frameon=False,
-        )
-        sns.despine(
-            offset=0,
-            trim=True,
-        );
-
-        # save the plot
-        fig.savefig(
-            plots_dir / "multi_resolution_image_correlation.png",
-            format="png",
-            dpi=300,
-            transparent=True,
-        )
-        
+        make_plot(data=df_final, save_loc=plot_loc)
         self.manifest = self.manifest.append(
-            {
-                "Description": "plot of similarity vs resolution",
-                "path": plots_dir / "multi_resolution_image_correlation.png",
-            },
+            {"Description": "plot of similarity vs resolution", "path": plot_loc},
             ignore_index=True,
         )
 
--- cvapipe/steps/multi_res_struct_compare/utils.py
+++ cvapipe/steps/multi_res_struct_compare/utils.py
@@ -1,6 +1,16 @@
+from pathlib import Path
+
 import numpy as np
+import pandas as pd
+
 from scipy.stats import pearsonr, gmean
 from skimage.measure import block_reduce
+from skimage.util import crop
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from aicsimageio import AICSImage
 
 
 def blockreduce_pyramid(input_arr, block_size=(2, 2, 2), func=np.max, max_iters=12):
@@ -73,24 +83,198 @@ def draw_pairs(input_list, n_pairs=1):
     """
     Draw unique (ordered) pairs of examples from input_list at random.
     Input list is not a list of pairs, just a list of single exemplars.
-    
+
     Example:
         >>> draw_pairs([0,1,2,3], n_pairs=3)
         >>> {(1,2), (2,3), (0,3)}
-        
+
     Note:
         A pair is only unique up to order, e.g. (1,2) == (2,1).  this function
-        only returns and compared sorted tuple to handle this 
+        only returns and compared sorted tuple to handle this
     """
-    
+
     # make sure requested number of uniquepairs in possible
     L = len(input_list)
-    assert n_pairs <= L*(L-1)/2
-    
-    # draw n_pairs of size 2 sets from input_list    
+    assert n_pairs <= L * (L - 1) / 2
+
+    # draw n_pairs of size 2 sets from input_list
     pairs = set()
     while len(pairs) < n_pairs:
-        pairs |= {frozenset(sorted(np.random.choice(input_list, 2 ,replace=False)))}
-    
+        pairs |= {frozenset(sorted(np.random.choice(input_list, 2, replace=False)))}
+
     # return a set of ordered tuples to not weird people out
     return {tuple(sorted(p)) for p in pairs}
+
+
+def make_masked_gfp_image(
+    image_path, crop_size=(64, 160, 96), gfp_channel_ind=4, cell_mask_channel_ind=1
+):
+    """
+    Take a path to a tiff and return the masked gfp 3d volume
+    """
+
+    # load image
+    image = AICSImage(image_path)
+    data_6d = image.data
+
+    # mask gfp by cell boundary
+    gfp_3d = data_6d[0, 0, 4, :, :, :]
+    mask_3d = data_6d[0, 0, 1, :, :, :]
+    gfp_3d[mask_3d == 0] = 0
+
+    # crop to desired shape
+    z_dim, y_dim, x_dim = gfp_3d.shape
+    z_desired, y_desired, x_desired = crop_size
+    z_crop = (z_dim - z_desired) // 2
+    y_crop = (y_dim - y_desired) // 2
+    x_crop = (x_dim - x_desired) // 2
+    gfp_3d = crop(gfp_3d, ((z_crop, z_crop), (y_crop, y_crop), (x_crop, x_crop)))
+    assert gfp_3d.shape == crop_size
+
+    return gfp_3d
+
+
+def compute_distance_metric(
+    row,
+    mdata_cols,
+    px_size=0.29,
+    crop_size=(64, 160, 96),
+    par_dir=Path("/allen/aics/modeling/ritvik/projects/actk/"),
+):
+    """
+    Main function to loop over in distributed
+    """
+
+    # get data for cells i and j
+    gfp_i = make_masked_gfp_image(par_dir / row.CellImage3DPath_i, crop_size=crop_size)
+    gfp_j = make_masked_gfp_image(par_dir / row.CellImage3DPath_j, crop_size=crop_size)
+
+    # multi-res comparison
+    pyr_corrs = pyramid_correlation(gfp_i, gfp_j, func=np.mean)
+
+    # comparison when one input is permuted (as baseline correlation)
+    gfp_i_shuf = gfp_i.copy().flatten()
+    np.random.shuffle(gfp_i_shuf)
+    gfp_i_shuf = gfp_i_shuf.reshape(gfp_i.shape)
+    pyr_corrs_permuted = pyramid_correlation(gfp_i_shuf, gfp_j, func=np.mean)
+
+    # grab correlations at each res in a df
+    df_tmp_corrs = pd.DataFrame()
+    for k, v in sorted(pyr_corrs.items()):
+        tmp_stat_dict = {
+            "Resolution (micrometers)": px_size * k,
+            "Pearson Correlation": v,
+            "Pearson Correlation permuted": pyr_corrs_permuted[k],
+        }
+        df_tmp_corrs = df_tmp_corrs.append(tmp_stat_dict, ignore_index=True)
+
+    # label stats with cell ids
+    df_tmp_corrs["CellId_i"] = row.CellId_i
+    df_tmp_corrs["CellId_j"] = row.CellId_j
+
+    # and append row metadata
+    df_row_tmp = row.to_frame().T
+    df_row_tmp = df_row_tmp.merge(df_tmp_corrs)
+
+    return df_row_tmp
+
+
+def make_pairs_df(
+    df_in,
+    structs=[
+        "Actin filaments",
+        "Mitochondria",
+        "Microtubules",
+        "Nuclear envelope",
+        "Desmosomes",
+        "Plasma membrane",
+        "Nucleolus (Granular Component)",
+        "Nuclear pores",
+    ],
+    N_pairs_per_struct=100,
+    mdata_cols=[
+        "StructureShortName",
+        "FOVId",
+        "CellIndex",
+        "CellId",
+        "StandardizedFOVPath",
+        "CellImage3DPath",
+    ],
+):
+    """
+    Make the pairwise df to loop over.
+    """
+    df_out = pd.DataFrame()
+    for struct in structs:
+        try:
+            df_struct = df_in[df_in.StructureShortName == struct]
+            pair_inds = draw_pairs(df_struct.index, n_pairs=N_pairs_per_struct)
+            for (i, j) in pair_inds:
+                row_i = df_struct.loc[i]
+                row_j = df_struct.loc[j]
+                row = (
+                    row_i[mdata_cols]
+                    .add_suffix("_i")
+                    .append(row_j[mdata_cols].add_suffix("_j"))
+                )
+                df_out = df_out.append(row, ignore_index=True)
+        except Exception as e:
+            print(
+                f"Not enough {struct} rows to get {N_pairs_per_struct} samples"
+                f" Error {e}."
+            )
+            break
+    assert np.all(df_out["CellId_i"] != df_out["CellId_j"])
+    df_out = df_out.reset_index(drop=True)
+    return df_out
+
+
+def clean_up_results(dist_metric_results):
+    """
+    Clean up distributed results.
+    """
+    df_final = pd.DataFrame()
+    for dataframes in dist_metric_results:
+        for corr_dataframe in dataframes:
+            if corr_dataframe is not None:
+                df_final = df_final.append(corr_dataframe)
+
+    # fix up final pairwise dataframe
+    df_final = df_final.reset_index(drop=True)
+    df_final = df_final.rename(
+        columns={"StructureShortName_i": "StructureShortName"}
+    ).drop(columns="StructureShortName_j")
+    df_final["Pearson Correlation gain over random"] = (
+        df_final["Pearson Correlation"] - df_final["Pearson Correlation permuted"]
+    )
+
+    return df_final
+
+
+def make_plot(data, save_loc):
+    """
+    Seaborn plot of mean corr gain over random vs resolution.
+    """
+    sns.set(style="ticks", rc={"lines.linewidth": 1.0})
+    fig = plt.figure(figsize=(10, 7))
+
+    ax = sns.pointplot(
+        x="Resolution (micrometers)",
+        y="Pearson Correlation gain over random",
+        hue="StructureShortName",
+        data=data,
+        ci=95,
+        capsize=0.2,
+        palette="Set2",
+    )
+    ax.legend(
+        loc="upper left", bbox_to_anchor=(0.05, 0.95), ncol=1, frameon=False,
+    )
+    sns.despine(
+        offset=0, trim=True,
+    )
+
+    # save the plot
+    fig.savefig(
+        save_loc, format="png", dpi=300, transparent=True,
+    )
